7744d757781e3d5321a5a0b0aafcddf01f2651bb
jeremysamuel13 <70209502+jeremysamuel13@users.noreply.github.com>
2022-11-07 18:44:51 -0500

    Delete Song

--- client/src/components/MUIRemoveSongModal.js
+++ client/src/components/MUIRemoveSongModal.js
@@ -37,7 +37,7 @@ export default function MUIRemoveSongModal() {
   }
 
   return (
-    <Modal open={store.listMarkedForDeletion !== null}>
+    <Modal open={store.isRemoveSongModalOpen()}>
       <Box sx={style}>
         <div
           id="remove-song-modal"
